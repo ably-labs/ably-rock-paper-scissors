927fdd892ae5c56d329256b11fad7078c8a6008b
tomczoink <tc13270@my.bristol.ac.uk>
2021-10-20 18:39:47 +0100

    Add ontouch events

--- public/game/game.js
+++ public/game/game.js
@@ -120,15 +120,27 @@ var mouseDown = 0;
 let gameCanvas = document.getElementById("game");
 gameCanvas.onmousedown = function(e) { 
   e.preventDefault();
-  ++mouseDown;
+  mouseDown = true;
 }
 document.body.onmouseup = function() {
-  --mouseDown;
+  mouseDown = false;
 }
 
 // $("#game").mousedown(function(event){
 //     event.preventDefault();
 // });
+var pointerX = 0;
+var pointerY = 0;
+
+document.onmousemove = function(event) {
+    pointerX = event.clientX;
+    pointerY = event.clientY;
+}
+
+const canvas = document.querySelector('canvas')
+canvas.addEventListener('mousedown', function(e) {
+    getCursorPosition(canvas, e)
+})
 
 function getCursorPosition(myPlayer, camera) {
     let canvasRect = gameCanvas.getBoundingClientRect();
@@ -159,18 +171,37 @@ function getCursorPosition(myPlayer, camera) {
     return mousePos;
 }
 
-var pointerX = 0;
-var pointerY = 0;
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+        mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
 
-document.onmousemove = function(event) {
-    pointerX = event.clientX;
-    pointerY = event.clientY;
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
 }
-
-const canvas = document.querySelector('canvas')
-canvas.addEventListener('mousedown', function(e) {
-    getCursorPosition(canvas, e)
-})
 
 Game.checkIfPlayerDied = async function() {
     let players = await this.ablyHandler.playerPositions();
